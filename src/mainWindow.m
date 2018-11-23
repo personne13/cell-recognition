e896a02205b00d5299cb5fa43ffcc76e8de39f41
personne13 <thomasdamiani13@gmail.com>
2018-11-23 16:36:51 +0100

    added main window

---
+++ src/mainWindow.m
@@ -0,0 +1,19 @@
+global mainWindow
+global widthWindow
+global heightWindow
+widthWindow = 1080;
+heightWindow = 620;
+
+mainWindow = uifigure('Position', [100, 30, widthWindow, heightWindow]);
+m = uimenu(mainWindow,'Text','File');
+mitem = uimenu(m,'Text','Open folder', 'Callback',@callback_openFolder);
+
+function callback_openFolder(src,evt)
+global mainWindow;
+global widthWindow;
+global heightWindow;
+dirpath = uigetdir('.');
+files = dir(dirpath);
+files.name
+p = uipanel(mainWindow,'Position',[20 20 100 (heightWindow - 60)]);
+end
